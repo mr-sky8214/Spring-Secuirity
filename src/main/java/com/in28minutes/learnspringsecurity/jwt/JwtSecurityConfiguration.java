3aaf13e27ec8266cced34a6b14867f2801bb48a1
Akash Kamble <kambleakash8214@gmail.com>
2024-02-07 23:08:19 +0530

    Added JWT using Spring Boot's OAuth2 Resource Server

--- src/main/java/com/in28minutes/learnspringsecurity/jwt/JwtSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learnspringsecurity/jwt/JwtSecurityConfiguration.java
@@ -1,25 +1,42 @@
-package com.in28minutes.learnspringsecurity.basicAuthentication;
+package com.in28minutes.learnspringsecurity.jwt;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+import java.util.UUID;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
-//@Configuration
-public class BasicAuthSecurityConfiguration {
+@Configuration
+public class JwtSecurityConfiguration {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -33,24 +50,9 @@ public class BasicAuthSecurityConfiguration {
         http.csrf().disable();
         http.headers().frameOptions().sameOrigin();
         http.httpBasic(withDefaults());
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user = User.withUsername("Akash")
-//                .password("{noop}sky")
-//                .roles(UserRole.USER.name())
-//                .build();
-//
-//        UserDetails admin = User.withUsername("admin")
-//                .password("{noop}sky")
-//                .roles(UserRole.ADMIN.name())
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
-
 
     @Bean
     public DataSource dataSource() {
@@ -66,19 +68,20 @@ public class BasicAuthSecurityConfiguration {
 //                .password("{noop}sky")
                 .password("sky")
                 .passwordEncoder(str -> passwordEncoder().encode(str))
-                .roles(UserRole.USER.name())
+                .roles("USER")
                 .build();
 
         UserDetails admin = User.withUsername("admin")
 //                .password("{noop}sky")
                 .password("sky")
                 .passwordEncoder(str -> passwordEncoder().encode(str))
-                .roles(UserRole.ADMIN.name())
+                .roles("ADMIN")
                 .build();
 
         var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
         jdbcUserDetailsManager.createUser(user);
         jdbcUserDetailsManager.createUser(admin);
+
 
         return jdbcUserDetailsManager;
     }
@@ -87,4 +90,54 @@ public class BasicAuthSecurityConfiguration {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+
+    /**
+     * JWT using Spring Boot's OAuth2 Resource Server - OAuth2ResourceServerConfigurer#jwt()
+     * 1. Create KeyPair -> KeyPairGenerator
+     * 2. Create RSA key object using Key Pair -> com.nimbusds.jose.jwk.RSAKey
+     * 3. Create JWKSource(JSON Web Source)
+     * 4. Create RSA public key for decoding
+     */
+
+    // 1. Create KeyPair -> KeyPairGenerator
+    @Bean
+    public KeyPair keyPair() {
+        try {
+            var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    //2. Create RSA key object using Key Pair -> com.nimbusds.jose.jwk.RSAKey
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair) {
+        return new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
+                .privateKey(keyPair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    //3. Create JWKSource(JSON Web Source)
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+        var jwkSet = new JWKSet(rsaKey);
+
+        // Overriding List<JWK> get(final JWKSelector jwkSelector, final C context)
+        return (jwkSelector, context) -> jwkSelector.select(jwkSet);
+    }
+
+    //4. Create RSA public key for decoding
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+        return NimbusJwtDecoder
+                .withPublicKey(rsaKey.toRSAPublicKey())
+                .build();
+    }
+
+
 }
