2becb95d90f493abfcd7f53051aef5483bf4a0b6
Akash Kamble <kambleakash8214@gmail.com>
2024-02-07 08:52:26 +0530

    Created In memory users

--- src/main/java/com/in28minutes/learnspringsecurity/basicAuthentication/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learnspringsecurity/basicAuthentication/BasicAuthSecurityConfiguration.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -26,6 +30,21 @@ public class BasicAuthSecurityConfiguration {
 
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("Akash")
+                .password("{noop}sky")
+                .roles(UserRole.USER.name())
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}sky")
+                .roles(UserRole.ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
---
+++ src/main/java/com/in28minutes/learnspringsecurity/basicAuthentication/UserRole.java
@@ -0,0 +1,6 @@
+package com.in28minutes.learnspringsecurity.basicAuthentication;
+
+public enum UserRole {
+    USER,
+    ADMIN
+}
